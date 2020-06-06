5befea49f80db111a4c73c0d5661987221d3014a
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2020-06-06 02:20:37 -0500

    First commit

---
+++ Cadenas de Markov/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cadenas_de_Markov {
+	class Program {
+		static void Main(string[] args) {
+			string str = "Sin duda alguna se trataba de un suicidio. De tal forma, que el coronel no quera ni oír hablar de otra hipótesis. Y la pobre señora Harker no tenía más consuelo que la compasión sincera de los vecinos. Las dos amigas, que la acompañaban en el día después del entierro, la encontraban en un estado lamentable. —Realmente, no comprendo cómo John ha podido hacer una cosa semejante cuando éramos tan felices —sollozaba—. Claro que su muerte hubiera sido menos incomprensible si yo no hubiera sido para él una esposa tierna y complaciente. Mejor que una esposa, un ángel guardián. Nuestra casa, por ejemplo, ¿creen que sería enteramente nuestra, con la hipoteca reembolsada hasta el último céntimo, si hubiera dejado obrar al buen John? Ni en un siglo lo habría conseguido. Desde las primeras semanas de nuestra vida en común, cuando me di cuenta de que le gustaba traerme flores, comprendí cual era mi deber: iba a tener que encargarme, yo sola, de la administración de nuestro presupuesto. Naturalmente, le daba cada semana un poco de dinero para sus gastos, y le compraba cada noche el periódico; él hubiera preferido comprarlo él mismo para leerlo en el tren, pero lo habría arrugado demasiado; ya saben, yo guardo todos los periódicos viejos, bien doblados, para venderlos al trapero. Evidentemente, si hubiéramos tenido hijos, no habría podido ocuparme tanto de él y de la casa. Pero antes de nuestra unión, el médico me había dicho que con mi constitución delicada, haría mejor en renunciar al terrible esfuerzo que puede representar la maternidad. Un hombre delicioso este médico. Como no tendrán bebés, mime usted a su marido, me aconsejó. John, protestó al principio, pero poco a poco se fue resignando. Sin embargo, no conseguía comprender por qué yo insistía en que su habitación estuviera tapizada de color rosa. Como me quedaba sola todo el día, me había puesto a coser; pronto supe confeccionar vestidos, e incluso camisas para John. Al principio me pedía que las comprara, pero yo le expliqué que me encantaba trabajar para él, puesto que, en suma, era mi bebé; y entonces acabó por no volver a hablarme de ello. Por supuesto, siempre estaba preocupada por su salud. Había comprado libros que trataban de todos los regímenes a seguir. Créanme, en veinte años de matrimonio, mi John no comió jamás un bocado que no conviniera exactamente a un hombre de su edad, de su peso y de su temperamento. Asimismo, velaba para que fuese siempre bien abrigado. Por la mañana, cuando el tiempo era lluvioso, le recordaba que tomara su impermeable, y, salvo en pleno verano, vigilaba que llevara su jersey de lana. Y cuando la mañana había sido hermosa, pero por la noche el cielo se había cubierto, iba a esperarle a la estación, con su impermeable, incluso cuando me sentía muy cansada. No imaginarían ustedes cuanto deseaba yo que todo marchara de modo impecable en nuestra casa. Y, no obstante, no es fácil cuando hay un hombre en la casa, especialmente un hombre como John. Necesité dos años enteros para acostumbrarle a que dejara sus zapatos en la alacena y se pusiera las zapatillas antes de entrar en la cocina. Para proteger la alfombra del salón, coloqué cuadrados de linóleo alrededor de su sillón preferido, y conservaba otros trozos como reserva. Cuando recibía a alguno amigos, que no tardaban en encender un cigarro, me precipitaba para deslizarles un trozo de linóleo debajo de los pies, por miedo a que ensuciaran la alfombra. Tuve que volver comprar linóleo varias veces. Yo había pasado de los treinta y empezaba a sentirme cansada, nerviosa; el médico me explicó que era un mal periodo el que debía atravesar, y que tenía que descansar. Así, pues, le pedí a John que refregara los platos en mi lugar, pero se mostraba tan descuidado que me vi obligada a poner trozos de linóleo delante de la fregadera de tanto que salpicaba el suelo. Ciertamente, yo sabía que él nunca se aburría conmigo, pero comprendía perfectamente que un hombre pudiera experimentar la necesidad de distraerse. Incluso insistí para que asistiese una vez al año a una reunión con sus amigos de regimiento. Cuando volvía, sus ropas olían de tal modo a tabaco que tenía que rociarlas de esencia de lavanda. Pero creo que les hablaba a sus compañeros de la solicitud con que yo le cuidaba. Para el entierro, enviaron una corona con un motivo central de margaritas que formaban las palabras: En paz. Una idea encantadora, ¿no les parece? Pero, ahora que lo pienso, sin duda les gustaría saber de qué modo ocurrió todo. Primero tengo que explicarles que a causa de mi delicada salud, teníamos habitaciones separadas. Como un esposo tiene, no obstante, ciertos derechos, yo nunca cerraba la puerta con llave. Debo decir que John era un hombre demasiado galante para aprovecharse de la situación. El mismo día de la boda le confié que, en opinión del doctor, cualquier agitación brutal podría matarme, y, por supuesto, como John conocía mi constitución delicada, no quería tener mi muerte sobre su conciencia. En su habitación yo haba colocado, a los pies de la cama, un trozo de linóleo, y sobre el linóleo un gran cenicero de porcelana china, blanco, con rosas amarillas. John era, a Dios gracias, demasiado refinado para mascar tabaco, ni siquiera para fumar; pero en cambio, le gustaba el chicle. Cada noche yo le daba uno, recordándole que lo dejara en el cenicero antes de dormirse. Pues bien, la noche de su muerte, antes de entregarle aquella golosina, le anuncié una buena noticia: reemplazando el café del desayuno por achicoria, había conseguido economizar tres dólares, justo el precio de un linóleo artstico que destinaba a su habitación. Se lo describí: sobre un fondo malva y rosa, un Cupido se disponía a lanzar su flecha sobre una gacela temblorosa de miedo, el símbolo perfecto de la pareja que forman el hombre conquistador y la mujer púdica. Con una gran decepción de mi parte, John no dijo nada; movió simplemente la cabeza, tomó el pedazo de chicle y fue a acostarse. Un poco más tarde, vi que apagaba la luz. Nuestras habitaciones eran continuas, saben ustedes, y la luz pasaba por debajo de la puerta. Le oí decir que me daba las buenas noches. Comprendí inmediatamente que algo andaba mal. Yo le haba enseñado a decir: Buenas noches, querida. Y en aquella ocasión dijo solo: Buenas noches. Luego, un poco más tarde, oí caer gotas. Debía ser la lluvia, o cualquier grifo tal vez. Llamé: —John, ¿has cerrado el agua caliente del cuarto de baño? Pero él se contentó con reír, una risa extraña, breve, entrecortada, y me dijo que no me preocupara. Las gotas seguían cayendo, espaciándose, no obstante, de modo que acabé por dormirme. A la mañana siguiente, cuando entré en la habitación para despertarle —era él quien preparaba el desayuno, con el fin de permitirme que me quedara acostada un rato más— lo encontré muerto. Se había abierto las venas con una hoja de afeitar. Lo que yo había tomado por la lluvia era el ruido de su vida que se iba, gota a gota. En mi enloquecimiento, llamé al doctor. Me explicó que John había debido tener una depresión nerviosa, casi una crisis de demencia. —No hay otra razón en un hombre que tiene la fortuna de tener una esposa tan devota como usted —dijo. Tenía ciertamente razón; no encuentro otra explicación. Evidentemente, John no había apreciado nunca todo lo que yo haca por él. Se sorprendía incluso, a veces, del trabajo que hacía para que la casa estuviera siempre bien conservada y limpia. Sobre este punto, él fue negligente hasta el fin. Si al menos hubiera pensado en acostarse un poco más hacia abajo, solo unos diez centímetros, su sangre habría caído sobre el linóleo, en lugar de manchar la alfombra.";
+			var tokens = str.Split(' ');
+			List<Node> lista = new List<Node>();
+
+			for (int i = 0; i < tokens.Length - 1; i++) { 
+				if ( lista.TrueForAll( n => n.n_gram != tokens[i]) ) {
+					var n = new Node(tokens[i]);
+					n.next_n_grams.Add(tokens[i + 1], 1);
+					lista.Add(n);
+				} else {
+					lista.ForEach(n => {
+						var a = tokens[i + 1];
+						if (n.n_gram == tokens[i]) {
+							n.total++;
+							if (!n.next_n_grams.ContainsKey(a)) {
+								n.next_n_grams.Add(a, 1);
+							} else {
+								n.next_n_grams[a] += 1;
+							}
+						}
+					});
+				}
+			}
+			Console.WriteLine($"{generate(lista)}\n");
+			Console.WriteLine($"{generate(lista)}\n");
+			Console.WriteLine($"{generate(lista)}\n");
+			Console.WriteLine("============= END =============");
+			Console.ReadLine();
+
+
+
+		}
+
+		public static string generate(List<Node> list) {
+			Random rand = new Random();
+			Node node = list[ rand.Next() % list.Count ];
+			string result = node.n_gram;
+			int length = rand.Next() % 10;
+
+			for (int i = 0; i < 243; i++) {
+
+				int random = rand.Next() % node.total;
+				var keys   = node.next_n_grams.Keys.ToList();
+				var values = node.next_n_grams.Values.ToList();
+				int j = 0;
+
+				while (values[j] < random) {
+					random -= values[j];
+					j++;
+				}
+				try {
+					node = list.First(n => n.n_gram == keys[j]);
+				} catch ( InvalidOperationException exception) {
+					result = result.Insert(result.Length, $".");
+					break;
+				}
+				
+				result = result.Insert(result.Length, $" {node.n_gram}");
+				
+
+			}
+
+			return result;
+		}
+
+
+	}
+
+	class Node {
+		public string n_gram { get; set; }
+		public int total { get; set; }
+		public Dictionary<string, int> next_n_grams { get; set; }
+		public Node( string n_gram) {
+			this.n_gram = n_gram;
+			this.total = 1;
+			this.next_n_grams = new Dictionary<string, int>();
+		}
+	}
+}
